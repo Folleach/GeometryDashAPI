7154bece2e7d54f9d662816ab89dff19bb198201
Folleach <folleach@yandex.ru>
2018-11-19 22:26:41 +0500

    Added project with examples.

---
+++ Examples/CustomBlockType.cs
@@ -0,0 +1,38 @@
+﻿using GeometryDashAPI.Data;
+using GeometryDashAPI.Levels;
+using GeometryDashAPI.Levels.Enums;
+using GeometryDashAPI.Levels.GameObjects.Default;
+
+namespace Examples
+{
+    //An example of creating your own classes for blocks.
+    class MyShakeTrigger : Trigger
+    {
+        public override Layer Default_ZLayer { get; protected set; } = Layer.T2;
+        public override short Default_ZOrder { get; protected set; } = 99;
+
+        public MyShakeTrigger() : base(1520)
+        {
+        }
+
+        public MyShakeTrigger(string[] data) : base(data)
+        {
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    class CustomBlockType
+    {
+        public static void Invoke()
+        {
+            LocalLevels levels = new LocalLevels();
+            BindingBlockID binding = new BindingBlockID();
+            binding.Bind(1520, typeof(MyShakeTrigger));
+            Level level = new Level(levels.GetLevelByName("Temp"), binding);
+        }
+    }
+}
---
+++ Examples/Program.cs
@@ -0,0 +1,20 @@
+﻿using GeometryDashAPI.Data;
+using GeometryDashAPI.Levels;
+using GeometryDashAPI.Levels.Interfaces;
+using System;
+
+namespace Examples
+{
+    //This class for the only test.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LocalLevels levels = new LocalLevels();
+            Level level = new Level(levels.GetLevelByName("Temp"));
+            foreach (IBlock element in level.Blocks)
+                Console.WriteLine(element.ID);
+            Console.ReadKey();
+        }
+    }
+}
