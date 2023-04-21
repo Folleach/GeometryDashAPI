a3a8844867ab7f430831d80f3a06c61645646731
Pawday <pawday@mail.ru>
2023-04-22 00:09:25 +0300

    Reimplement dropping index with method from projects library

--- GeometryDashAPI/Data/LocalLevels.cs
+++ GeometryDashAPI/Data/LocalLevels.cs
@@ -54,15 +54,13 @@ namespace GeometryDashAPI.Data
                 index = new Dictionary<string, Dictionary<int, int>>();
             else
                 index.Clear();
-            for (int i = 0; i < levels.Count; ++i)
+            
+            for (var i = 0; i < levels.Count; ++i)
             {
                 var level = levels[i];
-                if (!index.ContainsKey(level.Name)) 
-                    index.Add(level.Name, new Dictionary<int, int>());
-                // There is a chance for appearing more than one level with same name and revision
-                else if (index[level.Name].ContainsKey(level.Revision))
-                    continue; //Skip indexing level with same name and revision
-                index[level.Name].Add(level.Revision, i);
+                var revisionIndexMap = index.GetOrCreate(level.Name, _ => new Dictionary<int, int>());
+                if (!revisionIndexMap.ContainsKey(level.Revision))
+                    revisionIndexMap.Add(level.Revision, i);
             }
         }
 
