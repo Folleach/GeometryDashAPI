6e6f741603f79041283d8e27e598f714e0b6a996
flightlex <lolkekror0@gmail.com>
2023-05-08 17:04:04 +0300

    more and more triggers :)

---
+++ GeometryDashAPI/Levels/Structures/RgbColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryDashAPI.Levels.Structures
+{
+    public struct RgbColor
+    {
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+    }
+}
