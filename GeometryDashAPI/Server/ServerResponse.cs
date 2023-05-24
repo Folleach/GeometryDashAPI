f1419438d26f5b7fe5f07e8f8e0b3b58848434ba
Folleach <andrey@folleach.ru>
2023-05-24 20:25:02 +0500

    handle "error code" messages from server

--- GeometryDashAPI/Server/ServerResponse.cs
+++ GeometryDashAPI/Server/ServerResponse.cs
@@ -23,12 +23,14 @@ namespace GeometryDashAPI.Server
             HttpStatusCode = statusCode;
             raw = body;
             var match = StatusCodeMatcher.Match(body);
+            if (body.IndexOf("error code: ", StringComparison.Ordinal) == 0)
+                GeometryDashStatusCode = -1; // because "error code" looks like error from cloudflare (GeometryDash uses cloudflare as a proxy)
             if (match.Success)
                 GeometryDashStatusCode = int.Parse(match.Value);
             else
                 value = GeometryDashApi.Serializer.Decode<T>(body);
         }
-        
+
         public T GetResultOrDefault()
         {
             return value;
