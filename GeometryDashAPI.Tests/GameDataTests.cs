c32fe344cddb31bb5e95945078627fdd2980ad42
Folleach <andrey@folleach.ru>
2023-05-26 01:55:59 +0500

    add test for specific symbols in data

--- GeometryDashAPI.Tests/GameDataTests.cs
+++ GeometryDashAPI.Tests/GameDataTests.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using GeometryDashAPI.Data;
@@ -10,6 +11,9 @@ namespace GeometryDashAPI.Tests;
 public class GameDataTests
 {
     private const string PROCESS_TEMP_DATA_DIRECTORY = "GameDataTemp";
+
+    private static readonly string GameManager1Path = Path.Combine("data", "saves", "CCGameManager1.dat");
+    private static readonly string LocalLevels1Path = Path.Combine("data", "saves", "CCLocalLevels1.dat");
 
     [SetUp]
     public void SetUp()
@@ -28,7 +32,7 @@ public class GameDataTests
     [Test]
     public void LocalLevels_Load()
     {
-        var local = new LocalLevels(Path.Combine("data", "saves", "CCLocalLevels1.dat"));
+        var local = new LocalLevels(LocalLevels1Path);
 
         local.LevelExists("test1", 0).Should().Be(true);
         local.GetLevel("test1").Description.Should().Be("for tests");
@@ -37,7 +41,7 @@ public class GameDataTests
     [Test]
     public async Task LocalLevels_LoadAsync()
     {
-        var local = await LocalLevels.LoadFileAsync(Path.Combine("data", "saves", "CCLocalLevels1.dat"));
+        var local = await LocalLevels.LoadFileAsync(LocalLevels1Path);
 
         local.LevelExists("test1", 0).Should().Be(true);
         local.GetLevel("test1").Description.Should().Be("for tests");
@@ -46,7 +50,7 @@ public class GameDataTests
     [Test]
     public async Task LocalLevels_SaveAsync()
     {
-        var local = await LocalLevels.LoadFileAsync(Path.Combine("data", "saves", "CCLocalLevels1.dat"));
+        var local = await LocalLevels.LoadFileAsync(LocalLevels1Path);
         local.GetLevel("test1").Description = "new description";
 
         var fileName = Path.Combine(PROCESS_TEMP_DATA_DIRECTORY, "SaveAsync.dat");
@@ -59,7 +63,7 @@ public class GameDataTests
     [Test]
     public async Task LocalLevels_Save()
     {
-        var local = await LocalLevels.LoadFileAsync(Path.Combine("data", "saves", "CCLocalLevels1.dat"));
+        var local = await LocalLevels.LoadFileAsync(LocalLevels1Path);
         local.GetLevel("test1").Description = "new description";
 
         var fileName = Path.Combine(PROCESS_TEMP_DATA_DIRECTORY, "Save.dat");
@@ -73,7 +77,7 @@ public class GameDataTests
     [Test]
     public void GameManager_Load()
     {
-        var game = new GameManager(Path.Combine("data", "saves", "CCGameManager1.dat"));
+        var game = new GameManager(GameManager1Path);
 
         game.PlayerCube.Should().Be(1);
     }
@@ -81,7 +85,7 @@ public class GameDataTests
     [Test]
     public async Task GameManager_LoadAsync()
     {
-        var game = await GameManager.LoadFileAsync(Path.Combine("data", "saves", "CCGameManager1.dat"));
+        var game = await GameManager.LoadFileAsync(GameManager1Path);
 
         game.PlayerCube.Should().Be(1);
     }
@@ -89,7 +93,7 @@ public class GameDataTests
     [Test]
     public async Task GameManager_SaveAsync()
     {
-        var game = await GameManager.LoadFileAsync(Path.Combine("data", "saves", "CCGameManager1.dat"));
+        var game = await GameManager.LoadFileAsync(GameManager1Path);
         game.PlayerCube = 33;
         var fileName = Path.Combine(PROCESS_TEMP_DATA_DIRECTORY, "GameManagerSaveAsync.dat");
         await game.SaveAsync(fileName);
@@ -101,7 +105,7 @@ public class GameDataTests
     [Test]
     public async Task GameManager_Save()
     {
-        var game = await GameManager.LoadFileAsync(Path.Combine("data", "saves", "CCGameManager1.dat"));
+        var game = await GameManager.LoadFileAsync(GameManager1Path);
         game.PlayerCube = 33;
         var fileName = Path.Combine(PROCESS_TEMP_DATA_DIRECTORY, "GameManagerSaveAsync.dat");
         // ReSharper disable once MethodHasAsyncOverload
@@ -109,5 +113,20 @@ public class GameDataTests
 
         game = await GameManager.LoadFileAsync(fileName);
         game.PlayerCube.Should().Be(33);
+    }
+
+    [Test]
+    public async Task LocalLevels_SpecificSymbols()
+    {
+        var input = "& < > / \\ @";
+        var local = await LocalLevels.LoadFileAsync(LocalLevels1Path);
+        local.GetLevel("test1").Name = input;
+
+        var path = Path.Combine(PROCESS_TEMP_DATA_DIRECTORY, "SpecificSymbols.dat");
+        await local.SaveAsync(path);
+        local = await LocalLevels.LoadFileAsync(path);
+
+        local.LevelExists("test1").Should().BeFalse();
+        local.FirstOrDefault()?.Name.Should().Be(input);
     }
 }
