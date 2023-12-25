9dfe3a24f769d9fa11f445e92aeb4cb9a9243fbb
Folleach <andrey@folleach.ru>
2023-12-25 19:48:53 +0500

    add SongTrigger

---
+++ GeometryDashAPI/Levels/GameObjects/Triggers/SongTrigger.cs
@@ -0,0 +1,33 @@
+using GeometryDashAPI.Attributes;
+using GeometryDashAPI.Levels.GameObjects.Default;
+
+namespace GeometryDashAPI.Levels.GameObjects.Triggers;
+
+[GameBlock(1934)]
+public class SongTrigger : Trigger
+{
+    [GameProperty("392", 0)]
+    public int SongId { get; set; }
+
+    [GameProperty("399", false)]
+    public bool Prep { get; set; }
+
+    [GameProperty("400", false)]
+    public bool LoadPrep { get; set; }
+
+    [GameProperty("404", 0)]
+    public int Speed { get; set; }
+
+    [GameProperty("406", 1f, alwaysSet: true)]
+    public float Volume { get; set; } = 1f;
+
+    [GameProperty("413", false)]
+    public bool Loop { get; set; }
+
+    [GameProperty("432", 0)]
+    public int Channel { get; set; }
+
+    public SongTrigger() : base(1934)
+    {
+    }
+}
