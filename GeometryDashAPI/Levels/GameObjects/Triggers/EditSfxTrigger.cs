4206a1d99d0de974f1f2cce79c85155a9ef84cda
Folleach <andrey@folleach.ru>
2023-12-26 22:28:23 +0500

    add missing fields for edit sfx trigger

--- GeometryDashAPI/Levels/GameObjects/Triggers/EditSfxTrigger.cs
+++ GeometryDashAPI/Levels/GameObjects/Triggers/EditSfxTrigger.cs
@@ -9,6 +9,18 @@ public class EditSfxTrigger : Trigger
     [GameProperty("10", 0.5f)]
     public float Duration { get; set; } = 0.5f;
 
+    [GameProperty("51", 0)]
+    public int GroupId1 { get; set; }
+
+    [GameProperty("71", 0)]
+    public int GroupId2 { get; set; }
+
+    [GameProperty("138", false)]
+    public bool P1 { get; set; }
+
+    [GameProperty("200", false)]
+    public bool P2 { get; set; }
+
     [GameProperty("404", 0)]
     public int Speed { get; set; }
 
@@ -30,8 +42,32 @@ public class EditSfxTrigger : Trigger
     [GameProperty("419", false)]
     public bool ChangeSpeed { get; set; }
 
+    [GameProperty("421", 1f)]
+    public float VolNear { get; set; } = 1f;
+
+    [GameProperty("422", 0.5f)]
+    public float VolMed { get; set; } = 0.5f;
+
+    [GameProperty("423", 0f)]
+    public float VolFar { get; set; }
+
+    [GameProperty("424", 0f)]
+    public int MinDist { get; set; }
+
+    [GameProperty("425", 0f)]
+    public int Dist2 { get; set; }
+
+    [GameProperty("426", 0f)]
+    public int Dist3 { get; set; }
+
+    [GameProperty("428", false)]
+    public bool Cam { get; set; }
+
     [GameProperty("455", 0)]
     public int SfxGroup { get; set; }
+
+    [GameProperty("458", SoundPropagationDirection.All)]
+    public SoundPropagationDirection Propagation { get; set; }
 
     [GameProperty("457", 0)]
     public int GroupId { get; set; }
