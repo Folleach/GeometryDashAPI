09ebe81b4b700b1638a378be8150cd0951bb21ec
Folleach <andrey@folleach.ru>
2023-12-26 22:34:13 +0500

    add missing fields for song trigger

--- GeometryDashAPI/Levels/GameObjects/Triggers/SongTrigger.cs
+++ GeometryDashAPI/Levels/GameObjects/Triggers/SongTrigger.cs
@@ -21,6 +21,18 @@ public class SongTrigger : Trigger
     [GameProperty("406", 1f, alwaysSet: true)]
     public float Volume { get; set; } = 1f;
 
+    [GameProperty("408", 0)]
+    public int Start { get; set; }
+
+    [GameProperty("409", 0)]
+    public int FadeIn { get; set; }
+
+    [GameProperty("410", 0)]
+    public int End { get; set; }
+
+    [GameProperty("411", 0)]
+    public int FadeOut { get; set; }
+
     [GameProperty("413", false)]
     public bool Loop { get; set; }
 
